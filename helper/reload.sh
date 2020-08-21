8f35e2f7c57917a16f22b89c8062612f16628dea
Kaede Fujisaki <kaede@hexe.net>
2020-08-21 12:57:46 +0900

    rewrite alert

--- helper/reload.sh
+++ helper/reload.sh
@@ -5,5 +5,8 @@ BACKUP_FILENAME="backup-$(date '+%Y%m%d').tar.gz"
 PROJ_PATH=$(readlink -f $(cd $(dirname $(readlink -f $0)) && pwd))
 cd ${PROJ_PATH}/..
 
+set -x
 docker-compose exec prometheus kill -HUP 1
+docker-compose logs --tail=5 prometheus
 docker-compose exec alertmanager kill -HUP 1
+docker-compose logs --tail=5 alertmanager
