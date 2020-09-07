0c27f478f2ca7398098aac3fd4794751e723915d
Kaede Fujisaki <kaede@hexe.net>
2020-09-08 06:47:16 +0900

    stop using cluster.

--- helper/crate-down.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -eu
-
-PROJ_PATH=$(readlink -f $(cd $(dirname $(readlink -f $0)) && pwd))
-cd ${PROJ_PATH}/..
-
-set -x
-docker-compose stop crate-01 crate-02 crate-03 crate-adapter
-yes | docker-compose rm crate-01 crate-02 crate-03 crate-adapter
